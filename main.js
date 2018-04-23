105ea344f24acddc23e34c3fd7b33bb89e6278e5
unlocomqx <amxmodx.unloco@gmail.com>
2018-04-23 15:54:14 +0100

    build app

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win;
 
 function createWindow() {
 	// Create the browser window.
-	win = new BrowserWindow({width: 800, height: 600});
+	win = new BrowserWindow({width: 300, height: 123});
 	win.setMenu(null);
 
 	// and load the index.html of the app.
@@ -19,7 +19,7 @@ function createWindow() {
 	}));
 
 	// Open the DevTools.
-	win.webContents.openDevTools();
+	// win.webContents.openDevTools();
 
 	// Emitted when the window is closed.
 	win.on('closed', () => {
