f729c4b7da3f598304db76eb89dc42afb7e9f104
unlocomqx <amxmodx.unloco@gmail.com>
2020-12-16 13:49:09 +0100

    Remove tray icon when app is closed

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ function createWindow() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null;
+    tray.destroy();
   });
   win.on('minimize', () => win.hide());
 
