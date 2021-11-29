945504e2a6efdd180865cd51e5877579afb04dfc
unlocomqx <amxmodx.unloco@gmail.com>
2021-11-29 11:00:14 +0100

    Delete old js files

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const electron = require("electron");
-const {scaleFactor} = require("../src/stores/scale-factor");
-
-electron.ipcRenderer.on("scaleFactor", (event, value) => {
-  console.log(value);
-  scaleFactor.set(value);
-});
