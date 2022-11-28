8f24ea6cc0d5c423156418ecdd790b8829d46e72
unlocomqx <amxmodx.unloco@gmail.com>
2022-11-28 13:41:45 +0100

    upload file instead of string

--- src/tools/uploader.ts
+++ src/tools/uploader.ts
@@ -1,42 +1,43 @@
-import { prefs } from "../stores/prefs";
-import { progress } from "../stores/progress";
-import { ui } from "../stores/ui";
+import type { NativeImage } from "electron"
+import { prefs } from "../stores/prefs"
+import { progress } from "../stores/progress"
+import { ui } from "../stores/ui"
 
-export function uploadScreenshot (image) {
-  ui.setKey("status", null);
-  ui.setKey("screenshot_url", null);
+export function uploadScreenshot (image: NativeImage) {
+  ui.setKey("status", null)
+  ui.setKey("screenshot_url", null)
 
-  const form = new FormData();
-  form.append("file", image.toDataURL());
-  progress.set(0);
+  const form = new FormData()
+  form.append("file", new File([image.toJPEG(100)], "screenshot.jpg", { type: "image/jpg" }), "screenshot.jpg")
+  progress.set(0)
   window.axios.request({
     url             : prefs.getKey("upload_url"),
     method          : "POST",
     data            : form,
     onUploadProgress: (ev) => {
-      const percent = ev.loaded / ev.total * 100;
-      progress.set(percent);
+      const percent = ev.loaded / ev.total * 100
+      progress.set(percent)
     }
   }).then(res => {
-    progress.set(100);
-    const response = res.data;
+    progress.set(100)
+    const response = res.data
     if (response.startsWith("https://")) {
-      ui.setKey("screenshot_url", response);
+      ui.setKey("screenshot_url", response)
       if (prefs.getKey("copy_auto")) {
-        window.clipboard.writeText(response);
+        window.clipboard.writeText(response)
       }
     } else {
-      ui.setKey("screenshot_url", null);
-      ui.setKey("status", response);
+      ui.setKey("screenshot_url", null)
+      ui.setKey("status", response)
     }
-    playSound();
+    playSound()
   }).finally(() => {
-    progress.set(0);
-  });
+    progress.set(0)
+  })
 }
 
 function playSound () {
-  const audio = new Audio("assets/ding.mp3");
-  audio.volume = 0.2;
-  audio.play();
+  const audio = new Audio("assets/ding.mp3")
+  audio.volume = 0.2
+  audio.play()
 }
